ca1d801ce16141ec17fdee70f1ccac5cffa08c1d
Carlos Moreno <carlooos_28@hotmail.com>
2018-05-18 15:29:57 -0500

    feature: dynamic vote is added

--- src/components/App/candidatelist.js
+++ src/components/App/candidatelist.js
@@ -6,16 +6,58 @@ import { Row, Col, Button } from 'reactstrap';
 import HandUp from '../Icons/handup';
 import DownHand from '../Icons/downhand';
 
-class CandidateList extends Component {
+class CandidateList extends Component {	
+	state = {
+		upvote: false,
+		upbotton: false,
+		upcount: isNaN(localStorage.getItem('upcount_'+this.props.id)) ? 0 : Number(localStorage.getItem('upcount_'+this.props.id)),
+		downcount: isNaN(localStorage.getItem('downcount_'+this.props.id)) ? 0 : Number(localStorage.getItem('downcount_'+this.props.id)),
+	}	
+	handleUpVoteClick = (event) => {
+		this.setState({
+			upvote: true,
+			upbotton: true
+		})		
+	}	
+	handleDownVoteClick = (event) => {
+		this.setState({
+			upvote: false,
+			upbotton: true
+		})		
+	}	
+	onVote = (event) => {
+		if(this.state.upbotton) {
+			if(this.state.upvote) {		
+				let totalUpCount = Number(localStorage.getItem('upcount_'+this.props.id)) + 1;				
+				localStorage.setItem('upcount_'+this.props.id, totalUpCount)
+	
+				this.setState(prevState => ({
+					upcount: Number(localStorage.getItem('upcount_'+this.props.id)),
+					upbotton: false
+				}))		
+			
+			} else {
+				let totalDownCount = Number(localStorage.getItem('downcount_'+this.props.id)) + 1;
+				localStorage.setItem('downcount_'+this.props.id, totalDownCount);
+
+				this.setState(prevState => ({
+					downcount: Number(localStorage.getItem('downcount_'+this.props.id)),
+					upbotton: false
+				}))			
+			}
+		}
+	}
 	render() {
-		const divStyle = {
+		let divStyle = {
 			backgroundImage: 'url(images/' + this.props.image + ')',
 			height: '560px'			
-		  };		  
+		  },
+		    totalUpCount   = Math.round(this.state.upcount / (this.state.upcount + this.state.downcount)*100),
+			totalDownCount =  Math.round(this.state.downcount / (this.state.upcount + this.state.downcount)*100);
 
 		return (
 			<Col lg="6"  >
-				<div style={divStyle} ClassName="img-fluid">
+				<div style={divStyle} className="img-fluid">
 					<div className="Votes-character">
 					<div>
 				<p className="Title-character">	
@@ -33,17 +75,19 @@ class CandidateList extends Component {
 						{this.props.description}
 					</p>						
 					<p>
-						<span className="Votes-up"><HandUp size={16} color="#ffffff" /></span>
-						<span className="Votes-down"><DownHand size={16} color="#ffffff" /></span>
-						<Button className="btn-md btn btn-outline-dark">Vote now</Button>
+						<span className="Votes-up" onClick={this.handleUpVoteClick}><HandUp size={16} color="#ffffff" /></span>
+						<span className="Votes-down" onClick={this.handleDownVoteClick}><DownHand size={16} color="#ffffff" /></span>
+						<Button className="btn-md btn btn-outline-dark" onClick={this.onVote}>Vote now</Button>						 
 					</p>				
 				</section>					
 				<p className="Voting-result">
-						<span><HandUp size={28} color="#ffffff" /> 80% </span>
-						<span><DownHand size={28} color="#ffffff" /> 20% </span>
+						<span><HandUp size={28} color="#ffffff" />
+							{isNaN(totalUpCount) ? 0 : totalUpCount}% 
+						</span>
+						<span><DownHand size={28} color="#ffffff" /> 
+							{isNaN(totalDownCount) ? 0 : totalDownCount}% 
+						</span>
 					</p>										
-					{this.props.upvotes}
-					{this.props.downvotes}
 					</div></div>
 				</div>	
 			</Col>							
